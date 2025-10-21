327bda5b661639002aca40a03b1d528f899715b9
DanielCarrenoMar <dacarreno.24@est.ucab.edu.ve>
2025-10-20 22:24:25 -0400

    feat: incluir nombre

--- src/ui/layers/menuLayer.tsx
+++ src/ui/layers/menuLayer.tsx
@@ -11,7 +11,10 @@ export default function MenuLayer() {
     return (
     <div className="min-h-screen flex bg-gray-50">
         <LateralMenu role={userRole} />
-        <main className="flex-1 p-6">
+        <main className="flex-1 p-6 flex-col flex">
+            <header>
+                <h1 className="text-2xl font-bold mb-4">Bienvenido, {user?.name}</h1>
+            </header>
             <Outlet />
         </main>
     </div>
